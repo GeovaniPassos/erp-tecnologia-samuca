0fcde9031f3332773a5c9ebfd988626a7e1366c7
Geovani Passos <geovanipassos95@outrlook.com>
2024-12-16 23:21:34 -0300

    Inicio do projeto ERP com Samuca

---
+++ src/main/java/br/com/cursodetecnologia/erp_tecnologia/ErpTecnologiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cursodetecnologia.erp_tecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpTecnologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpTecnologiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cursodetecnologia/erp_tecnologia/ErpTecnologiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cursodetecnologia.erp_tecnologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpTecnologiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
